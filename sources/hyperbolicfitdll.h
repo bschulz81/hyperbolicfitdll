427f50e547307092d0aa32a9cdea2c4962bcacde
bschulz81 <80183710+bschulz81@users.noreply.github.com>
2021-05-16 18:23:00 +0200

    Update hyperbolicfitdll.h

--- sources/hyperbolicfitdll.h
+++ sources/hyperbolicfitdll.h
@@ -75,7 +75,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-
 #pragma once
 
 #ifdef APTHYPERBOLICFITDLL_EXPORTS
@@ -99,7 +98,7 @@ using namespace std;
 // function is to terminate successfully
 
 // main_error is a pointer to a double value. If the pointer is not NULL, the value of main_error will be the sum of the squares of the differences between a square of the the best fit and a square of the measurement data. 
-// The errors involve two squares because we calculate the squared error of a fit where the hyperbola was converted to a line.
+// Yes, the errors involve two squares because we calculate the squared error of a fit where the hyperbola was converted to a line.
 
 
 // main_slope and main_intercept are pointers to double values. If the pointers are not NULL, their values will be the slope and intercept of a line given by
@@ -120,13 +119,13 @@ using namespace std;
 // if its error from the initial fit is deemed not to be an outlier based on various statistical methods. A new fit with the added point is then made and the process is repeated with another initial combination of points.
 // The algorithm works in parallel with several threads. So it benefits from processors with multiple cores.
 // 
-// The initial combination is selected randomly if the binominal coefficient of the number of points n and the number of outliers k (n choose k) is larger than 22 choose 11 = 705432. Otherwise, the combinations are searched deterministically.
+// The initial combination is selected randomly if the binominal coefficient of the number of points n and the number of outliers k (n choose k) is larger than 100*(22 choose 11) = 70543200. Otherwise, the combinations are searched deterministically.
 // 
 // stop_after_seconds is a parameter that stops the RANSAC after a given time in seconds has elapsed.
 // stop_after_numberofiterations_without_improvement is a parameter that lets the RANSAC stop after it has iterated by stop_after_numberofiterations_without_improvement iterations
 // without a further improvement of the error. Note that this parameter is not the iteration number, but it is the number of iterations without further improvement.
 // 
-// The parameters stop_after_seconds and stop_after_numberofiterations_without_improvement are only used if the binominal coefficient n choose k is larger than 22 choose 11 == 705432.
+// The parameters stop_after_seconds and stop_after_numberofiterations_without_improvement are only used if the binominal coefficient n choose k is larger than 100*(22 choose 11) == 70543200.
 
 
 // backslash is a parameter that can contain the focuser backslash in steps. The best focus position is corrected with respect to this backslash. If you already have taken account of
