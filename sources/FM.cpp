a9811afce12ef0f4519f3028c3214b03d3e515f4
bschulz81 <80183710+bschulz81@users.noreply.github.com>
2021-08-31 10:46:48 +0200

    Add files via upload
    changed a file path that would work only on windows to a mechanism that should be agnostic of the operating system

--- sources/FM.cpp
+++ sources/FM.cpp
@@ -273,6 +273,36 @@ int main(int argc, char** argv)
 	long maxiterations = 2000000;
 	double scale = 1.0;
 	bool toleranceset = false;
+	if (argc == 1)
+	{
+		cout << "not enough command line arguments"<< endl;
+		cout << "This is the documentation of the test application of the focus interpolation library. The test application expects the following arguments" << endl;
+		cout << "-d Directoryname  or --Directory Directoryname, where Directoryname is the path to a folder with fits files" << endl;
+		cout << "-w  or --Write_to_file if the application should document its output in a file called Directoryname\\results.txt" << endl;
+		cout << "-info number or --Infolevel number, where number ranges from 0 [default] to 3, which sets the level of information that the application prints. 0 means it only prints out the focus point. 3 means it prints the full information" << endl;
+		cout << "-m or --Median if the curve fit is done with Siegel's median fitting. The default is false because the median slope is somewhat slow" << endl;
+		cout << "-sc number or --Scale number, where scale is a floating point number >=1, that enlarges the area where the focus point is searched. The default is 1" << endl;
+		cout << "-bs number or --Backslash number, where number is of type long and should be a previously measured focuser backslash that is subtracted from the estimated focus position. The default is 0. " << endl;
+		cout << "-sec number or --Seconds_for_ransac number  where number is of type double and >0. It specifies the number of seconds after which the ransac is stopped. The default is 60" << endl;
+		cout << "-it number or --Iterations_for_ransac number where number is of type long and >0. It specifies the number of iterations without improvement after which the ransac is stopped. The default is 2000000" << endl;
+		cout << "-mo number or --Maximum_number_of_outliers number, where number is of type long>=0. It specifies the largest number of outliers that the algorithm can find. The default is the number n of supplied images minus 7. if n<7, then the number of outliers it can find is set to 0. If the binomial coefficient of n and n-8 is larger than 7054320, then the default number of outliers is given by the largest number k where the binomial coefficient of n,k is smaller than 7054320." << endl;
+		cout << "-tol number or --Tolerance number, where number is a value of type float. It sets a cut off value for the estimators. For all estimators the default is 3.0, except for the estumators Significance_in_Grubbs_test, where it is 0.2,Standard_deviation, where it is 2.0" << endl;
+		cout << "-e ESTIMATOR or --Estimator ESTIMATOR defines the estimator for the outlier removal procedure. ESTIMATOR can be one of the following variables:" << endl;
+		cout << "S or 5 This mmeans the S estimator is used, which is the default. The tolerance is the cut-off  in |squared_error-average_squared_error|/S(squared_errors)<=tolerance" << endl;
+		cout << "ALL This mmeans that, provided the maximum number of outliers is >0) several interpolations are made where most of the estimators are tried and the different results noted. For the standard deviation, a tolderance of 2 is used, and for the Grubbs test, a tolerance value of 0.2 is used. For the other estimators, the user supplied value or the default 3 is used. If the maximum number of outliers is 0, then only simple linear regression and the median slope are tested." << endl;
+		cout << "NO_REJECTION or 0 This means that no outliers are removed and the tolerance value is ignored. The same behavior can be set by using -mo 0" << endl;
+		cout << "MAXIMUM_SQUARED_ERROR or 1 This means the tolerance is the maximum squared error between the fit and the measured points" << endl;
+		cout << "STANDARD_DEVIATION or 2 this means that outliers are rejected if their squared_error with respect to the fit is larger than average_squared_error+tolerance*standard_deviation_of_squared_errors" << endl;
+		cout << "SIGNIFICANCE_IN_GRUBBS_TEST or 3 This means that the tolerance value defines the significance level in the Grubbs test. " << endl;
+		cout << "MAD or 4. This means that the MAD estimator is used and the tolerance is the cut-off  in |squared_error-average_squared_error|/MAD(squared_errors)<=tolerance" << endl;
+		cout << "Q or 6 This means that the Q estimator is used and the tolerance is the cut-off in |squared_error-average_squared_error|/Q(squared_errors)<=tolerance" << endl;
+		cout << "T or 7 This means that the T estimator is used and the tolerance is the cut-off in |squared_error-average_squared_error|/T(squared_errors)<=tolerance" << endl;
+		cout << "PEIRCE_CRITERION or 8 This means that the Peirce criterion is used" << endl;
+		cout << "BIWEIGHT_MIDVARIANCE or 9 This means that the biweight midvariance estimator is used and the tolerance is the cut-off in |squared_error-average_squared_error|/biweight-midvariance(squared_errors)<=tolerance" << endl;
+		cout << "PERCENTAGE_BASED_MIDVARIANCE or 10 This means that the percentage-based midvariance estimator is used and the tolerance is the cut-off in |squared_error-average_squared_error|/percentage_based_midvariance(squared_errors)<=tolerance" << endl;
+		cout << "For example a basic usage of this program may be FM.exe -d Path_To_my_fits_files -info 3 -e ALL -w This would analyze the fits files in the given directory. It will make several curve fits for most of the estimators available in the library and write the results into a file." << endl;
+		return 0;
+	}
 	for (size_t i = 1; i < argc; i++)
 	{
 		if (((string)argv[i] == "-h") || ((string)argv[i] == "--Help"))
@@ -466,9 +496,9 @@ int main(int argc, char** argv)
 
 	if (writetofile)
 	{
-		string filename_out = directorypath;
-		filename_out.append("\\results.txt");
-		myfile1.open(filename_out);
+		fs::path filename_out = directorypath;
+		filename_out /="results.txt";
+		myfile1.open(filename_out.c_str());
 		if (myfile1.is_open() == false)
 		{
 			cout << "could not open an output file";
